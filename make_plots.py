a7c367ac951542a8e6f3fa5347332cd7365b7c72
Kai Hwang <kai.hwang@gmail.com>
2022-01-16 13:21:47 -0600

    indiv sub PCA.:

--- make_plots.py
+++ make_plots.py
@@ -1,4 +1,5 @@
 #### script to make various plots for this project
+import pwd
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -8,22 +9,93 @@ sns.set_palette("colorblind")
 import setup
 from thalpy.constants import paths
 from matplotlib import pyplot as plt
-from thalpy.analysis import pc, plotting, feature_extraction
-from thalpy import masks
-from thalpy import fc
-from thalpy import base
+from thalpy.analysis import pc, plotting, feature_extraction, glm
+from thalpy import masks, fc, base
 import nibabel as nib
-from thalpy import glm
 import os
+from IPython.display import display, HTML
+plt.ion()
 
 # path setup
 MDTB_DIR = "/mnt/nfs/lss/lss_kahwang_hpc/data/MDTB/"
 MDTB_DIR_TREE = base.DirectoryTree(MDTB_DIR)
 MDTB_ANALYSIS_DIR = MDTB_DIR + 'analysis/'
 
+def run_pca(matrix, dir_tree, output_name, task_list, masker=None):
+    currnet_path=os.getcwd()
+    os.chdir(dir_tree.analysis_dir)
+    pca, loadings, correlated_loadings, explained_var = feature_extraction.compute_PCA(matrix, output_name=output_name, var_list=task_list, masker=masker)
+    
+    # for MDTB 
+    # pca_comps[:, 1] = pca_comps[:, 1] * -1
+    # loadings[:, 1] = loadings[:, 1] * -1
+    # correlated_loadings[1] = correlated_loadings[1] * -1
+
+    loadings_df = pd.DataFrame(loadings, index=task_list)
+    summary_df = loadings_df.describe()
+    var_df = loadings_df.var(axis=0).rename("var")
+    summary_df = summary_df.append(var_df)
+    #display(HTML(summary_df.to_html()))
+    os.chdir(currnet_path)
+    return pca, loadings, explained_var
+
 ################################################
-######## PCA analaysis
+######## PCA analaysis, Figure 1
 ################################################
+#### Tomoya N&N dataset
+tomoya_dir_tree = base.DirectoryTree('/mnt/nfs/lss/lss_kahwang_hpc/data/Tomoya')
+subjects = base.get_subjects(tomoya_dir_tree.deconvolve_dir, tomoya_dir_tree)
+stim_config_df = pd.read_csv(tomoya_dir_tree.deconvolve_dir + paths.STIM_CONFIG)
+TOMOYA_TASKS = stim_config_df["Stim Label"].tolist()
+tomoya_masker = masks.binary_masker(masks.MOREL_PATH)
+tomoya_masker.fit(nib.load('/mnt/nfs/lss/lss_kahwang_hpc/data/Tomoya/3dDeconvolve/sub-01/FIRmodel_MNI_stats.nii'))
+tomoya_beta_matrix = glm.load_brik(subjects, tomoya_masker, 'FIRmodel_MNI_stats.nii', 2227, TOMOYA_TASKS, zscore = False)
+
+#demean
+# tomoya_beta_mean = np.mean(tomoya_beta_matrix, axis=0)
+# tomoya_beta_new = np.empty([2227,104,6])
+# for i in np.arange(2227):
+#     tomoya_beta_new[i,:,:] = tomoya_beta_matrix[i,:,:] - tomoya_beta_mean
+
+# tomoya_beta_mean = np.mean(tomoya_beta_new, axis=1)
+# tomoya_beta_dm = np.empty([2227,104,6])
+# for i in np.arange(104):
+#     tomoya_beta_dm[:,i,:] = tomoya_beta_new[:,i,:] - tomoya_beta_mean
+    
+# z-score
+tomoya_beta_matrix = glm.zscore_subject_2d(tomoya_beta_matrix)
+tomoya_beta_matrix[tomoya_beta_matrix>3] = 0
+tomoya_beta_matrix[tomoya_beta_matrix<-3] = 0
+
+#run pca
+averaged_sub_beta_tomoya = np.mean(tomoya_beta_matrix, axis=2)
+tomoya_pca_comps, tomoya_loadings, tomoya_explained_ave_var = run_pca(averaged_sub_beta_tomoya, tomoya_dir_tree, 'tomoya_pca_groupave', TOMOYA_TASKS, masker=tomoya_masker)
+plt.close('all')
+
+#tomoya_explained_var = []
+tomoya_pca_WxV = np.zeros([2227, 6])
+tomoya_explained_var = pd.DataFrame()
+#np.zeros([10,6])
+
+for s in np.arange(tomoya_beta_matrix.shape[2]):
+    mat = tomoya_beta_matrix[:,:,s]
+    fn = 'tomoya_pca_sub' + str(s)
+    comps, _, var = run_pca(mat, tomoya_dir_tree, fn, TOMOYA_TASKS, masker=tomoya_masker)
+    plt.close('all')
+    tdf = pd.DataFrame()
+    tdf['Component'] = np.arange(1,11)
+    tdf['Dataset'] = 'N&N'
+    tdf['Sub'] = s
+    tdf['Varaince Explained'] = var[0:10] #varianced explained for the first 10 PC
+    tdf['Sum of Variance Explained'] = tdf['Varaince Explained'].cumsum()
+    tomoya_explained_var = tomoya_explained_var.append(tdf) 
+    for i in np.arange(10):
+        tomoya_pca_WxV[:,s] = tomoya_pca_WxV[:,s] + abs(comps[:,i])*var[i] #each subjects PCAweight * variance explained
+       
+tomoya_pca_weight = tomoya_masker.inverse_transform(np.mean(tomoya_pca_WxV, axis=1))  #average across subjects
+nib.save(tomoya_pca_weight, "images/tomoya_pca_weight.nii.gz")
+
+#### MDTB dataset
 stim_config_df = pd.read_csv(MDTB_DIR + paths.DECONVOLVE_DIR + paths.STIM_CONFIG)
 CONDITIONS_LIST = stim_config_df["Stim Label"].tolist()
 
@@ -33,35 +105,42 @@ mdtb_masker.fit(img)
 
 TASK_LIST = list(set(stim_config_df["Group"].to_list()))
 TASK_LIST.remove("Rest")
+#load zscored betas
+mdtb_beta_matrix = np.load(MDTB_ANALYSIS_DIR + 'mdtb_task_zscored.npy') #check with Evan on how this is generated, used different function than Tomoya?
+mdtb_beta_matrix[mdtb_beta_matrix>3] = 0
+mdtb_beta_matrix[mdtb_beta_matrix<-3] = 0
 
-from thalpy.analysis import glm
+#PCA
+mdtb_pca_comps, mdtb_loadings, mdtb_explained_var = run_pca(np.mean(mdtb_beta_matrix, axis=2), MDTB_DIR_TREE, 'mdtb_pca_groupave', TASK_LIST, masker=mdtb_masker)
+plt.close('all')
 
-tomoya_dir_tree = base.DirectoryTree('/mnt/nfs/lss/lss_kahwang_hpc/data/Tomoya')
-subjects = base.get_subjects(tomoya_dir_tree.deconvolve_dir, tomoya_dir_tree)
-stim_config_df = pd.read_csv(tomoya_dir_tree.deconvolve_dir + paths.STIM_CONFIG)
-TOMOYA_TASKS = stim_config_df["Stim Label"].tolist()
-tomoya_masker = masks.binary_masker(masks.MOREL_PATH)
-tomoya_masker.fit(nib.load('/mnt/nfs/lss/lss_kahwang_hpc/data/Tomoya/3dDeconvolve/sub-01/FIRmodel_MNI_stats.nii'))
-tomoya_beta_matrix = glm.load_brik(subjects, tomoya_masker, 'FIRmodel_MNI_stats.nii', 2227, TOMOYA_TASKS, zscore = False)
+mdtb_explained_var = pd.DataFrame()
+mdtb_pca_WxV = np.zeros([2227, 21])
+#mdtb_explained_var = np.zeros([10,21])
+for s in np.arange(mdtb_beta_matrix.shape[2]):
+    mat = mdtb_beta_matrix[:,:,s]
+    fn = 'mdtb_pca_sub' + str(s)
+    comps, _, var = run_pca(mat, MDTB_DIR_TREE, fn, TASK_LIST, masker=mdtb_masker)
+    plt.close('all')
+    tdf = pd.DataFrame()
+    tdf['Component'] = np.arange(1,11)
+    tdf['Dataset'] = 'MDTB'
+    tdf['Sub'] = s
+    tdf['Varaince Explained'] = var[0:10] #varianced explained for the first 10 PC
+    tdf['Sum of Variance Explained'] = tdf['Varaince Explained'].cumsum()
+    mdtb_explained_var = mdtb_explained_var.append(tdf)
+    for i in np.arange(10):
+        mdtb_pca_WxV[:,s] = mdtb_pca_WxV[:,s] + abs(comps[:,i])*var[i] #each subjects PCAweight * variance explained
 
+mdtb_pca_weight = mdtb_masker.inverse_transform(np.mean(mdtb_pca_WxV, axis=1)) #average across subjects
+nib.save(mdtb_pca_weight, "images/mdtb_pca_weight.nii.gz")
 
+######## plot weight x var
+plotting.plot_thal(tomoya_pca_weight)
+plotting.plot_thal(mdtb_pca_weight)
 
-################################################
 ######## Varaince explained plot
-################################################
-# plot PC variances explained
-df1 = pd.DataFrame()
-df1['Component'] = np.arange(1,11)
-df1['Dataset'] = 'MDTB'
-df1['Varaince Explained'] = np.array([0.49422765, 0.22373263, 0.07304692, 0.05084266, 0.03473523, 0.02276043, 0.01554752, 0.01323701, 0.01273611, 0.00932534])
-df1['Sum of Variance Explained'] = df1['Varaince Explained'].cumsum()
-
-df2 = pd.DataFrame()
-df2['Component'] = np.arange(1,11)
-df2['Dataset'] = 'N&N'
-df2['Varaince Explained'] = np.array([0.24283326, 0.13583435, 0.1266729,  0.04636843, 0.03969473, 0.03290696, 0.02337343, 0.02231827, 0.0147305,  0.01280407])
-df2['Sum of Variance Explained'] = df2['Varaince Explained'].cumsum()
-df = df1.append(df2)
+df = mdtb_explained_var.append(tomoya_explained_var)
 df['Component'] = df['Component'].astype('str')
 
 g = sns.barplot(data=df, x="Component", y="Varaince Explained", hue='Dataset')
@@ -69,10 +148,13 @@ g.get_legend().remove()
 g2 = g.twinx()
 g2 = sns.lineplot(data=df, x="Component", y="Sum of Variance Explained", hue='Dataset', legend = False)
 fig = plt.gcf()
-fig.set_size_inches([4,3])
+fig.set_size_inches([4,4])
 fig.tight_layout()
-fig.savefig("/home/kahwang/RDSS/tmp/pcave.png")
+fig.savefig("/home/kahwang/RDSS/tmp/pcvarexp.png")
 
-3dcalc -a tomoya_pca_component_0.nii -b tomoya_pca_component_7.nii -c tomoya_pca_component_9.nii -d tomoya_pca_component_3.nii -e tomoya_pca_component_4.nii -f tomoya_pca_component_5.nii -g tomoya_pca_component_6.nii -expr "(abs(a) + abs(b)*3 + abs(c)*3 + abs(d) + abs(e) + abs(f) + abs(g)*10)/10" -prefix tomoya_pca_weight.nii.gz
 
+
+################################################
+######## Task hubs versus rsfc hubs, Figure 2
+################################################
 
