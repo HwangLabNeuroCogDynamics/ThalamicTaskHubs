ea5808f433bf4d17da2c6d68d02d1ae74cd6c4e3
Kai Hwang <kai.hwang@gmail.com>
2022-05-25 15:59:12 -0500

    pca reg for fc.

--- create_FCmat.py
+++ create_FCmat.py
@@ -7,6 +7,9 @@ import nibabel as nib
 from nilearn import image, input_data, masking
 import nilearn.image
 from nilearn.image import resample_to_img, index_img
+from scipy import stats, linalg
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
 
 def generate_correlation_mat(x, y):
 	"""Correlate each n with each m.
@@ -35,6 +38,23 @@ def generate_correlation_mat(x, y):
 	return cov / np.dot(s_x[:, np.newaxis], s_y[np.newaxis, :])
 
 
+def pca_reg_fc(source_ts, target_ts):
+	'''use PCA regression to calculate FC, inputs are time by ROI'''
+	ts_len = target_ts.shape[0]
+	roi_size = target_ts.shape[1]
+	subcortical_size = source_ts.shape[1]
+	n_comps = np.amin([ts_len, roi_size, subcortical_size]) // 20
+	pca = PCA(n_comps)
+	reduced_mat = pca.fit_transform(source_ts) # Time X components
+	components = pca.components_
+	regrmodel = LinearRegression()
+	reg = regrmodel.fit(reduced_mat, target_ts) #cortex ts also time by ROI
+	#project regression betas from component
+	fcmat = pca.inverse_transform(reg.coef_).T #reshape to cortex
+
+	return fcmat 
+
+
 mdtb_dir = '/Shared/lss_kahwang_hpc/data/MDTB/'
 tomoya_dir = '/Shared/lss_kahwang_hpc/data/Tomoya/'
 
@@ -48,35 +68,37 @@ mni_thalamus_masker = input_data.NiftiMasker(mni_thalamus_mask)
 
 mdtb_fcmats = np.zeros((2445, 400, len(mdtb_subs)))
 for i, sub in enumerate(mdtb_subs):
-    func_file = nib.load(mdtb_dir+"3dDeconvolve/" + sub + "/FIRmodel_errts_block.nii.gz")
-    
-    cortical_ts = Schaefer400_masker.fit_transform(func_file)
-    roi_ts = mni_thalamus_masker.fit_transform(func_file) #roi_ts = data[np.nonzero(mni_thalamus_mask.get_fdata())]
-    
-    roi_ts = np.delete(roi_ts, np.where(roi_ts.mean(axis=1)==0)[0], axis=0)
-    cortical_ts = np.delete(cortical_ts, np.where(cortical_ts.mean(axis=1)==0)[0], axis=0)
-    
-    fcmat = generate_correlation_mat(roi_ts.T, cortical_ts.T)
-    fcmat[np.isnan(fcmat)] = 0
-    mdtb_fcmats[:,:,i] = fcmat
+	func_file = nib.load(mdtb_dir+"3dDeconvolve/" + sub + "/FIRmodel_errts_block.nii.gz")
+	
+	cortical_ts = Schaefer400_masker.fit_transform(func_file)
+	roi_ts = mni_thalamus_masker.fit_transform(func_file) #roi_ts = data[np.nonzero(mni_thalamus_mask.get_fdata())]
 
-np.save(mdtb_dir + "analysis/mdtb_fcmats", mdtb_fcmats)
+	roi_ts = np.delete(roi_ts, np.where(roi_ts.mean(axis=1)==0)[0], axis=0)
+	cortical_ts = np.delete(cortical_ts, np.where(cortical_ts.mean(axis=1)==0)[0], axis=0)
+	
+	#fcmat = generate_correlation_mat(roi_ts.T, cortical_ts.T)
+	fcmat = pca_reg_fc(roi_ts, cortical_ts)
+	fcmat[np.isnan(fcmat)] = 0
+	mdtb_fcmats[:,:,i] = fcmat
+
+np.save(mdtb_dir + "analysis/mdtb_pcafcmats", mdtb_fcmats)
 
 tomoya_fcmats = np.zeros((2445, 400, len(tomoya_subs)))
 for i, sub in enumerate(tomoya_subs):
-    func_file = nib.load(tomoya_dir+"3dDeconvolve/" + sub + "/FIRmodel_errts.nii.gz")
-    
-    cortical_ts = Schaefer400_masker.fit_transform(func_file)
-    roi_ts = mni_thalamus_masker.fit_transform(func_file)#data[np.nonzero(mni_thalamus_mask.get_fdata())]
-    
-    roi_ts = np.delete(roi_ts, np.where(roi_ts.mean(axis=1)==0)[0], axis=0)
-    cortical_ts = np.delete(cortical_ts, np.where(cortical_ts.mean(axis=1)==0)[0], axis=0)
-    
-    fcmat = generate_correlation_mat(roi_ts, cortical_ts.T)
-    fcmat[np.isnan(fcmat)] = 0
-    tomoya_fcmats[:,:,i] = fcmat
+	func_file = nib.load(tomoya_dir+"3dDeconvolve/" + sub + "/FIRmodel_errts.nii.gz")
+	
+	cortical_ts = Schaefer400_masker.fit_transform(func_file)
+	roi_ts = mni_thalamus_masker.fit_transform(func_file)#data[np.nonzero(mni_thalamus_mask.get_fdata())]
+	
+	roi_ts = np.delete(roi_ts, np.where(roi_ts.mean(axis=1)==0)[0], axis=0)
+	cortical_ts = np.delete(cortical_ts, np.where(cortical_ts.mean(axis=1)==0)[0], axis=0)
+	
+	#fcmat = generate_correlation_mat(roi_ts, cortical_ts.T)
+	fcmat = pca_reg_fc(roi_ts, cortical_ts)
+	fcmat[np.isnan(fcmat)] = 0
+	tomoya_fcmats[:,:,i] = fcmat
 
-np.save(tomoya_dir + "analysis/tomoya_fcmats", tomoya_fcmats)
+np.save(tomoya_dir + "analysis/tomoya_pcafcmats", tomoya_fcmats)
 
 
 
